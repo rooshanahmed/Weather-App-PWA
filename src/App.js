146ab7196f7e14e0d358eeada56b8451d71b21ea
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-09-13 23:07:48 +0500

    App Updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { fetchWeather } from './API/fetchWeather';
+import './App.css';
 
 const App = () => {
     return (
-        <h1>App</h1>
+        <div className="main-container">
+            <input 
+                type="text"
+                className="search"
+                placeholder="Search..."
+                value={}
+                onChange={}
+            />
+        </div>
     )
 }
 export default App
