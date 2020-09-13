23717e8f19713b6af378b3b65caaa2cd94ca74b9
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-09-13 23:14:13 +0500

    App Updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { fetchWeather } from './API/fetchWeather';
 import './App.css';
 
 const App = () => {
+    const [query, setQuery] = useState('');
+
+    const search = async (e) => {
+        if(e.key === 'Enter'){
+            const data = await fetchWeather(query);
+
+            console.log(data);
+        }
+    }
+
     return (
         <div className="main-container">
             <input 
                 type="text"
                 className="search"
                 placeholder="Search..."
-                value={}
-                onChange={}
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyPress={search}
             />
         </div>
     )
