0ecd77aa92665cfdaa4581266de6fc313b3de3af
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-09-14 15:44:46 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { fetchWeather } from "./API/fetchWeather";
-import { TextField } from '@material-ui/core';
+import { TextField } from "@material-ui/core";
 import "./App.css";
 
 const App = () => {
@@ -17,38 +17,40 @@ const App = () => {
   };
 
   return (
-    <div className="main-container">
-      <TextField 
-        type="text"
-        variant="filled"
-        label="Search Weather"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyPress={search}
-        style={{ backgroundColor: "white" , borderColor: "teal" }}
-      />
-      <br />
-      {weather.main && (
-        <div className="city">
-          <h2 className="city-name">
-            <span>{weather.name}</span>
-            <sup>{weather.sys.country}</sup>
-          </h2>
-          <div className="city-temp">
-            {Math.round(weather.main.temp)}
-            <sup>&deg;C</sup>
+    <>
+      <div className="main-container">
+        <TextField
+          type="text"
+          variant="filled"
+          label="Search Weather"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyPress={search}
+          style={{ backgroundColor: "white", borderColor: "teal" }}
+        />
+        <br />
+        {weather.main && (
+          <div className="city">
+            <h2 className="city-name">
+              <span>{weather.name}</span>
+              <sup>{weather.sys.country}</sup>
+            </h2>
+            <div className="city-temp">
+              {Math.round(weather.main.temp)}
+              <sup>&deg;C</sup>
+            </div>
+            <div className="info">
+              <img
+                className="city-icon"
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt={weather.weather[0].description}
+              />
+              <p>{weather.weather[0].description}</p>
+            </div>
           </div>
-          <div className="info">
-            <img
-              className="city-icon"
-              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-              alt={weather.weather[0].description}
-            />
-            <p>{weather.weather[0].description}</p>
-          </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 };
 export default App;
